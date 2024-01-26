444568fc72592244eced6dbe3f93e453a00332f9
Vladimir Novikov <NovikovVV@users.noreply.github.com>
2024-01-26 17:01:20 +0300

    Add files via upload

--- source/js/script.js
+++ source/js/script.js
@@ -3,6 +3,11 @@ const menuToggle = document.querySelector('.menu__toggle');
 const menu = document.querySelector('.menu');
 const menuIconClosed = '<svg width="50" height="24"><use xlink:href="#menu-closed"></use></svg>';
 const menuIconOpened = '<svg width="23" height="23"><use xlink:href="#menu-opened"></use></svg>';
+const priceSlider = document.querySelector('.price__slider');
+
+const middlePoint = (priceSlider.scrollWidth - priceSlider.clientWidth) / 2;
+priceSlider.scrollLeft = middlePoint;
+
 
 menuToggle.addEventListener('click', function () {
   menu.classList.toggle('menu--opened');
