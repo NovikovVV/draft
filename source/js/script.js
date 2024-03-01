77b0caa7515fd550fb6853f5705b3fa86f59449d
Vladimir Novikov <NovikovVV@users.noreply.github.com>
2024-03-01 15:50:37 +0300

    Add files via upload

--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,8 @@
 const navigationContainer = document.querySelector('.navigation .container');
 const menuToggle = document.querySelector('.menu__toggle');
 const menu = document.querySelector('.menu');
-const menuIconClosed = '<svg width="50" height="24"><use xlink:href="#menu-closed"></use></svg>';
-const menuIconOpened = '<svg width="23" height="23"><use xlink:href="#menu-opened"></use></svg>';
+const menuIconClosed = '<svg width="50" height="24"><use xlink:href="img/sprite.svg#menu-closed"></use></svg>';
+const menuIconOpened = '<svg width="23" height="23"><use xlink:href="img/sprite.svg#menu-opened"></use></svg>';
 
 menuToggle.addEventListener('click', function () {
   menu.classList.toggle('menu--opened');
